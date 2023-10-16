7de768740e2bc1abe481ca8072f7af9f39801075
Panaria <baranyldzby@gmail.com>
2023-10-16 20:34:14 +0300

    Add project files.

---
+++ wforms dort islem/Form1.cs
@@ -0,0 +1,68 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace wforms_dort_islem
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            int sayi1, sayi2, toplam;
+
+            sayi1 = Convert.ToInt32(textBox1.Text);
+            sayi2 = Convert.ToInt32(textBox2.Text);
+
+            toplam = sayi1 + sayi2;
+
+            MessageBox.Show("Sonuç = " + toplam);
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            int sayi1, sayi2, fark;
+
+            sayi1 = Convert.ToInt32(textBox1.Text);
+            sayi2 = Convert.ToInt32(textBox2.Text);
+
+            fark = sayi1 - sayi2;
+
+            MessageBox.Show("Sonuç = " + fark);
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            int sayi1, sayi2, carpim;
+
+            sayi1 = Convert.ToInt32(textBox1.Text);
+            sayi2 = Convert.ToInt32(textBox2.Text);
+
+            carpim = sayi1 * sayi2;
+
+            MessageBox.Show("Sonuç = " + carpim);
+        }
+
+        private void button4_Click(object sender, EventArgs e)
+        {
+            double sayi1, sayi2, bolum;
+
+            sayi1 = Convert.ToInt32(textBox1.Text);
+            sayi2 = Convert.ToInt32(textBox2.Text);
+
+            bolum = sayi1 / sayi2;
+
+            MessageBox.Show("Sonuç = " + bolum);
+        }
+    }
+}
